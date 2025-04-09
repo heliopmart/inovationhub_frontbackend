031328fb5a67bd6ce573fb71e7987c439a20e678
Hélio Martins <helioperesmartinsneto@gmail.com>
2025-04-09 17:57:28 -0300

    fixed last commit

--- lib/authManager.ts
+++ lib/authManager.ts
@@ -1,32 +1,32 @@
-import {TokenVerifyProps} from "@/types/interfaceClass"
-import { NextRequest, NextResponse } from 'next/server';
-import { getSignedCookie, setSignedCookie } from './cookie';
-import {
-  createPublicToken,
-  createPrivateToken,
-  validatePublicToken,
-  validatePrivateToken
-} from './tokenManager';
+// import {TokenVerifyProps} from "@/types/interfaceClass"
+// import { NextRequest, NextResponse } from 'next/server';
+// import { getSignedCookie, setSignedCookie } from './cookie';
+// import {
+//   createPublicToken,
+//   createPrivateToken,
+//   validatePublicToken,
+//   validatePrivateToken
+// } from './tokenManager';
 
-export async function handleAuth(req: NextRequest, res: NextResponse) {
-  // 1. Validar ou criar token público
-  const publicHash = getSignedCookie(req, 'public_token');
-  let publicData = publicHash ? await validatePublicToken(publicHash) : null;
+// export async function handleAuth(req: NextRequest, res: NextResponse) {
+//   // 1. Validar ou criar token público
+//   const publicHash = getSignedCookie(req, 'public_token');
+//   let publicData = publicHash ? await validatePublicToken(publicHash) : null;
 
-  if (!publicData) {
-    const { hash, token } = await createPublicToken();
-    setSignedCookie(res, 'public_token', hash, 60 * 60 * 24 * 5); // 5 dias
-    publicData = { token, allowedTables };
-  }
+//   if (!publicData) {
+//     const { hash, token } = await createPublicToken();
+//     setSignedCookie(res, 'public_token', hash, 60 * 60 * 24 * 5); // 5 dias
+//     publicData = { token, allowedTables };
+//   }
 
-  // 2. Validar token privado
-  const userId = getSignedCookie(req, 'private_token');
-  let userData = userId ? await validatePrivateToken(userId) : null;
+//   // 2. Validar token privado
+//   const userId = getSignedCookie(req, 'private_token');
+//   let userData = userId ? await validatePrivateToken(userId) : null;
 
-  if (!userData) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
+//   if (!userData) {
+//     return NextResponse.redirect(new URL('/login', req.url));
+//   }
 
-  // 3. Retornar dados válidos
-  return { public: publicData, private: userData };
-}
+//   // 3. Retornar dados válidos
+//   return { public: publicData, private: userData };
+// }
